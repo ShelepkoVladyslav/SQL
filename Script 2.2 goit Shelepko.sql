ec9354e57452913e85a288674404a761c470f26e
ShelepkoVladyslav <159441079+ShelepkoVladyslav@users.noreply.github.com>
2024-02-22 08:40:13 +0200

    Delete Script 2.2 goit Shelepko.sql

--- Script 2.2 goit Shelepko.sql
+++
@@ -1,8 +0,0 @@
-select campaign_id,
-sum(value) as value_,
-sum(spend) as spend_,
-(sum(value) - sum(spend)) / sum(spend) :: decimal as romi
-from facebook_ads_basic_daily
-group by campaign_id
-having sum(spend) > 500000
-order by romi desc
